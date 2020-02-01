676b59af061be2afd2ebb0a5abe65d6e6f92c2da
emilyrn18 <emilyrn18@vt.edu>
2020-02-01 13:08:08 -0500

    got the data

--- worms.py
+++ worms.py
@@ -13,7 +13,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 # Connects to  Client ID, SECRET, and username
 cid = '5239ca0b44d74a8188734bae8ad6e330'
 secret = '6df461582d434e01bdfb7bba529d9b16'
-#username = "12139911168"
+
+# username = "12139911168"
 username = sys.argv[1]
 
 # applies your client ID and secret
@@ -24,7 +25,7 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 # Get read access to your library
 scope = 'user-library-read user-read-private user-read-playback-state user-modify-playback-state'
 
-#token = 'BQDWkJXMVemN_F9uzDtLLwOeLqWgM3-sR8qphkfS3Zw09aPVA7ZIFn0uNkU_hOjQ2Xn-8nXJwwsj-6Ch1ytJExgInfpc2i5fnRu6oLaQll2gAdDUqeCaj9VMLjqKTfa6mXpYzCLggzAGD8P2cps'
+# token = 'BQDWkJXMVemN_F9uzDtLLwOeLqWgM3-sR8qphkfS3Zw09aPVA7ZIFn0uNkU_hOjQ2Xn-8nXJwwsj-6Ch1ytJExgInfpc2i5fnRu6oLaQll2gAdDUqeCaj9VMLjqKTfa6mXpYzCLggzAGD8P2cps'
 
 # Obtains token based on your username and scope
 try:
@@ -41,22 +42,23 @@ else:
 
 # grabs available spotify devices
 devices = spotifyObject.devices()
-print(json.dumps(devices, sort_keys=True, indent=4))
+# 1print(json.dumps(devices, sort_keys=True, indent=4))
 deviceID = devices['devices'][0]['id']
 
 # Get track information
 track = spotifyObject.current_user_playing_track()
-print(json.dumps(track, sort_keys=True, indent=4))
+trackID = track['item']['id']
+#print(json.dumps(track, sort_keys=True, indent=4))
 print()
-print(track)
 artist = track['item']['artists'][0]['name']
 track = track['item']['name']
-
+# print(track['item']['id'])
 if (len(artist) > 0):
     print("Currently playing " + artist + " - " + track)
 
 # Analyze current track
-
+soundData = spotifyObject.audio_analysis(trackID)
+print(json.dumps(soundData, sort_keys=True, indent=4))
 
 # Spotify interactivity
 choice = "0"
