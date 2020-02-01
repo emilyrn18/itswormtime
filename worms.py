97a567f81c8e073c80e2924722627807b1be6814
emilyrn18 <emilyrn18@vt.edu>
2020-02-01 10:59:23 -0500

    obtain access to api

--- worms.py
+++ worms.py
@@ -1,4 +1,9 @@
-# Imports
+# Import libraries
+import os
+import sys
+import json
+import webbrowser
+from json.decoder import JSONDecodeError
 import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -6,18 +11,27 @@ from spotipy.oauth2 import SpotifyClientCredentials
 # Access token: BQDWkJXMVemN_F9uzDtLLwOeLqWgM3-sR8qphkfS3Zw09aPVA7ZIFn0uNkU_hOjQ2Xn-8nXJwwsj-6Ch1ytJExgInfpc2i5fnRu6oLaQll2gAdDUqeCaj9VMLjqKTfa6mXpYzCLggzAGD8P2cps
 
 # Connects to  Client ID, SECRET, and username
-cid = '<INSERT CLIENT ID>'
-secret = '<INSERT CLIENT SECRET>'
-username = ""
+cid = '5239ca0b44d74a8188734bae8ad6e330'
+secret = '6df461582d434e01bdfb7bba529d9b16'
+#username = "12139911168"
+username = sys.argv[1]
 
 client_credentials_manager = SpotifyClientCredentials(
     client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # Get read access to your library
-scope = 'user-library-read'
-token = util.prompt_for_user_token(username, scope)
+scope = 'user-library-read user-read-private user-read-playback-state user-modify-playback-state'
+
+#token = 'BQDWkJXMVemN_F9uzDtLLwOeLqWgM3-sR8qphkfS3Zw09aPVA7ZIFn0uNkU_hOjQ2Xn-8nXJwwsj-6Ch1ytJExgInfpc2i5fnRu6oLaQll2gAdDUqeCaj9VMLjqKTfa6mXpYzCLggzAGD8P2cps'
+
+try:
+    token = util.prompt_for_user_token(username, scope)
+except (AttributeError, JSONDecodeError):
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username, scope)
+
 if token:
-    sp = spotipy.Spotify(auth=token)
+    spotifyObject = spotipy.Spotify(auth=token)
 else:
     print("Can't get token for", username)
